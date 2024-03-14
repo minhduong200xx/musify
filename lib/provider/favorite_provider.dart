bdf9c35cb687a68692046269f59dd0ddea657f43
minhthangchan3110 <thangdeptraivodich@gmail.com>
2024-03-14 14:51:29 +0700

    update-provider-favorite

--- lib/provider/favorite_provider.dart
+++ lib/provider/favorite_provider.dart
@@ -15,13 +15,13 @@ class FavoriteSongsProvider extends ChangeNotifier {
     } else {
       _favoriteSongs.add(song);
     }
-    _applyFilter(""); // Reset filter after toggle
+    _applyFilter("");
     notifyListeners();
   }
 
   void removeFromFavorites(Song song) {
     _favoriteSongs.remove(song);
-    _applyFilter(""); // Reset filter after removal
+    _applyFilter("");
     notifyListeners();
   }
 
